d7a9ef0b25683c7774340370cff12194b86777f3
belien <2782694792@qq.com>
2023-05-28 16:04:02 +0800

    fix - #if 宏定义编译异常

--- ExportScannerInfo/belien/log.h
+++ ExportScannerInfo/belien/log.h
@@ -4,31 +4,30 @@
 #include <ctime>
 
 static std::string getTime() {
-    const char* time_fmt = "%Y-%m-%d %H:%M:%S";
-    time_t      t        = time(nullptr);
-    char        time_str[64];
-    strftime(time_str, sizeof(time_str), time_fmt, localtime(&t));
+	const char* time_fmt = "%Y-%m-%d %H:%M:%S";
+	time_t      t = time(nullptr);
+	char        time_str[64];
+	strftime(time_str, sizeof(time_str), time_fmt, localtime(&t));
 
-    return time_str;
+	return time_str;
 }
 
-namespace belien {
-namespace log {
-	
+//namespace belien {
+//	namespace log {
+
 #if BELIEN_QT==0
 #define LOGI(format, ...)                                                   \
     fprintf(stderr, "[INFO]%s [%s:%d %s()] " format "\n", getTime().data(), \
             __FILE__, __LINE__, __func__, ##__VA_ARGS__)
 #define LOGI_(...)                                                   \
     fprintf(stderr, "[INFO]%s [%s:%d %s()] %s"\n", getTime().data(), \
-            __FILE__, __LINE__, __func__, ##__VA_ARGS__)
+	            __FILE__, __LINE__, __func__, ##__VA_ARGS__)
 #define LOGE(format, ...)                                                    \
     fprintf(stderr, "[ERROR]%s [%s:%d %s()] " format "\n", getTime().data(), \
             __FILE__, __LINE__, __func__, ##__VA_ARGS__)
 #define LOGE_(...)                                                    \
     fprintf(stderr, "[ERROR]%s [%s:%d %s()] %s\n", getTime().data(), \
             __FILE__, __LINE__, __func__, ##__VA_ARGS__)
-
 #elif BELIEN_QT==1
 #define LOGI(format, ...)                                                    \
     qDebug("[INFO]%s [%s:%d %s()] " format "\n", getTime().data(), __FILE__, \
@@ -44,7 +43,7 @@ namespace log {
            __LINE__, __FUNCTION__, ##__VA_ARGS__)
 #endif
 
-} // namespace log
-} // namespace belien
+//	} // namespace log
+//} // namespace belien
 
 #endif
